3ce92a964bc2d8215b3f7a4f13e33873e9031969
ask <askbk@protonmail.com>
2018-10-19 15:35:27 +0200

    Added comments.

--- Oving5/led_board.py
+++ Oving5/led_board.py
@@ -5,7 +5,9 @@ import random
 
 class LED_Board:
     def __init__(self):
+        # Output pins
         self.pins = [19, 13, 6]
+        # Pin states
         self.pin_led_states = [
                 [1, 0, -1], # 1
                 [0, 1, -1], # 2
@@ -15,11 +17,13 @@ class LED_Board:
                 [-1, 0, 1]  # 6
             ]
 
+    # GPIO setup for hver pin
     def setup(self):
         GPIO.setmode(GPIO.BCM)
         for i in range(0, 3):
             GPIO.setup(self.pins[i], GPIO.OUT)
 
+    # Sett en pin til riktig state
     def set_pin(self, pin_index, pin_state):
         if pin_state == -1:
             GPIO.setup(self.pins[pin_index], GPIO.IN)
@@ -27,22 +31,19 @@ class LED_Board:
             GPIO.setup(self.pins[pin_index], GPIO.OUT)
             GPIO.output(self.pins[pin_index], pin_state)
 
+    # Skru av alle leds
     def clear_leds(self):
         # Skru foerst av alle LEDs
         for i in range(0, 3):
             GPIO.setup(self.pins[i], GPIO.OUT)
             GPIO.output(self.pins[i], GPIO.LOW)
 
+    # Skru på en LED
     def light_led(self, led_number):
         for pin_index, pin_state in enumerate(self.pin_led_states[led_number]):
             self.set_pin(pin_index, pin_state)
 
-    #def light_led(self, light):
-        #self.clear_leds()
-        # Skru paa den valgte LEDen
-        #for i in range(0,3):
-            #self.set_pin(i, light)
-
+    # Flash leds som feilmelding
     def flash_all_leds(self):
         for i in range(0, 6):
             for j in range(1, 10000):
@@ -50,12 +51,14 @@ class LED_Board:
             self.clear_leds()
             time.sleep(.200)
     
+    # Blinke alle leds som positiv tilbakemelding
     def twinkle_all_leds(self):
         for i in range(0, 20):
             self.light_led(random.randint(0, 5))
             time.sleep(.100)
         self.clear_leds()
 
+    # lysshow når man skrur av 
     def power_off(self):
         for i in range(6, 0, -1):
             for dur in range(0, 10000):
@@ -63,6 +66,7 @@ class LED_Board:
                     self.light_led(j)
         self.clear_leds()
 
+    # lysshow når man skrur på 
     def power_on(self):
         for i in range(1, 7):
             for j in range(0, 10000):
