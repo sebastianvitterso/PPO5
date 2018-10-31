4323a38b91c1a6a967179cc3315597d4f04c10fd
ask <askbk@protonmail.com>
2018-10-31 15:04:50 +0100

    nutet litt

--- oving6/behavior.py
+++ oving6/behavior.py
@@ -1,4 +1,4 @@
-class Behaviour:
+class Behavior:
     def __init__(self, bbcon, sensobs, halt_request, priority):
         self.bbcon = bbcon
         self.sensobs = sensobs
@@ -9,17 +9,23 @@ class Behaviour:
         self.match_degree = 0
         self.weight = 0
 
+    # Dersom en oppførsel er aktiv, skal den vurdere om den skal deaktivere seg selv
     def consider_deactivation(self):
         pass
-
+    
+    # Dersom en oppførsel er inaktiv, skal den vurdere som den skal aktivere seg selv
     def consider_activation(self):
         pass
 
     # Hovedgrensesnitt mellom bbcon og behaviour
     def update(self):
-        pass
+        # Oppdater activity status her
 
+        self.sense_and_act()
+
+        self.weight = self.match_degree * self.priority
+
+    # Gjør beregninger for å produsere motoranbefalinger og oppdatere match_degree
     def sense_and_act(self):
         pass
-
 
