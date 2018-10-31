148d4de79140fa13697427969cd6b19e3e734051
Kobrestad <kaare@obrestad.net>
2018-10-31 15:04:19 +0100

    Sensob

--- oving6/camera.py
+++ oving6/camera.py
@@ -10,7 +10,8 @@ class Camera():
         self.img_height = img_height
         self.img_rot = img_rot
 
-    def get_value(self):  return self.value
+    def get_value(self):
+        return self.value
 
     def update(self):
         self.sensor_get_value()
